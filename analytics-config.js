ce779057363710c6c020f2d3b1514385109598ce
whoisqcm <whoisqcm@users.noreply.github.com>
2026-04-04 14:57:58 +0800

    Remove manual analytics scripts for Cloudflare setup

--- analytics-config.js
+++
@@ -1,5 +0,0 @@
-window.CF_WEB_ANALYTICS = {
-  // Paste the public site token from Cloudflare Web Analytics > Manage site > JS snippet.
-  // If your site uses Cloudflare automatic injection or Pages one-click setup, leave this blank.
-  token: ""
-};
